3313fb135d9a7dfdd98ed3b664a7382e24bd66a8
yahya <yahyak@gmail.com>
2017-07-03 11:25:03 +0300

    N/A

--- NodeJSPharmacy/server.js
+++ NodeJSPharmacy/server.js
@@ -1,1 +1,11 @@
-﻿
+﻿var http = require("http");
+
+var express = require("express");
+var app = express();
+
+//Setup view engine
+app.set("view engine", "vash");
+
+//Setup server
+var server = http.createServer(app);
+server.listen(3000);
